dfcf72775bb8545a5cf5ab2989160048aba5c405
shantanu400 <ashunayan400@gmail.com>
2024-07-09 14:42:30 +0530

    first commmit

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,11 +1,27 @@
-import React from 'react';
+// import React from 'react';
 
+// const ColourSelector = (props) => {
+//   const { config, selectNextBackground } = props
+//   const { background,className,key,label } = config;
+//   return (
+//     <button className={className} data-testid={key} onClick={() => selectNextBackground({background: config.background})}>
+//      {label}
+//     </button>
+//   )
+// }
+// export default ColourSelector;
+
+
+import React from 'react';
+ 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
-  const { background } = config;
+ const { config, selectNextBackground } = props
+ 
+  const { background, classname, label, key } = config;
+ 
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
+    <button className={classname}  data-testid={key} onClick={() => selectNextBackground({background: background})}>
+      {label}
     </button>
   )
 }
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,38 @@
+// import React, { useState } from "react"
 
+// export default function Selection(props){
+//   const { applyColor } = props;
+//   const [style, setStyle] = useState({background: ""});
+
+//   const handleClick = () => {
+//     applyColor(setStyle);
+//   };
+
+//   return(
+//     <div className="fix-box" style={style} onClick={handleClick}>
+//       selection
+//     </div>
+//   )
+// }
+
+import React, { useState } from 'react';
+ 
+const Selection = ({ applyColor }) => {
+    const [style, setStyle] = useState({ background: "" });;
+ 
+    const handleClick = () => {
+      applyColor(setStyle);
+    };
+ 
+ 
+ 
+  return (
+    <div className="fix-box" style={style} onClick={handleClick}>
+      Selection
+    </div>
+ 
+  );
+};
+ 
+ 
+export default Selection;
